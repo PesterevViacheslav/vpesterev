151654092c8ba1841f0f527c676d56fbf2e4dbba
PesterevViacheslav <pesterevvv@gmail.com>
2018-03-05 23:29:49 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,9 +1,19 @@
 package ru.job4j;
 
-public class Calculate
-{
-  public static void main(String[] args)
-  {
+/**
+* Class Calculate решение задачи Части 001. Базовый синтаксис урок 1.
+*
+* @author Viacheslav Pesterev (pesterevvv@gmail.com)
+* @since 03.03.2018
+* @version 1
+*/
+public class Calculate {
+
+  /**
+    * Method main. Вывод строки в консоль. 
+    * @param  args - args.
+    */
+  public static void main(String[] args) {
     System.out.println("Hello, World!!!!!"); 
   }
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Viacheslav Pesterev (pesterevvv@gmail.com)
+* @since 03.03.2018
+* @version 1
+*/
+package ru.job4j;
