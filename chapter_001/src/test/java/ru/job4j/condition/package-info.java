dbf804d59cc154cd08af70aafb39fbd252fe38d0
PesterevViacheslav <pesterevvv@gmail.com>
2018-03-18 19:48:51 +0300

    3.4. Расстояние между точками в системе координат [#188]

---
+++ chapter_001/src/test/java/ru/job4j/condition/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Пакет автотестов для решение задачи Части 001. Базовый синтаксис урок 3.4.
+ *
+ * @author Viacheslav Pesterev (pesterevvv@gmail.com)
+ * @since 18.03.2018
+ * @version 1
+ */
+package ru.job4j.condition;
