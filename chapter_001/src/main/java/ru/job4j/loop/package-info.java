89972efc44e9514a5ba0d0d1b494cb00ed6c771c
PesterevViacheslav <pesterevvv@gmail.com>
2018-03-23 23:37:06 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

---
+++ chapter_001/src/main/java/ru/job4j/loop/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Пекет для решение задачи Части 001. Базовый синтаксис урок 5.
+ *
+ * @author Viacheslav Pesterev (pesterevvv@gmail.com)
+ * @since 23.03.2018
+ * @version 1
+ */
+package ru.job4j.loop;
