e0793add00df4fae03f03aa5c5f48400a477cc23
PesterevViacheslav <pesterevvv@gmail.com>
2018-03-25 12:25:32 +0300

    5.3. Построить шахматную доску в псевдографике. [#13559]

---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class BoardTest Автотесты для задач Части 001. Базовый синтаксис урок 5.
+ *
+ * @author Viacheslav Pesterev (pesterevvv@gmail.com)
+ * @since 25.03.2018
+ * @version 1
+ */
+public class BoardTest {
+    /**
+     * Тест операции получения псевдографики шахматной доски размером 5x5
+     */
+    @Test
+    public void checkPaintFiveOnFiveBoard() {
+        Board brd = new Board();
+        String ln = System.lineSeparator();
+        String result = brd.paint(5, 5);
+        assertThat(result, is(String.format("X X X%s X X %sX X X%s X X %sX X X%s", ln, ln, ln, ln, ln)));
+    }
+
+    /**
+     * Тест операции получения псевдографики шахматной доски размером 3x5
+     */
+    @Test
+    public void checkPaintThreeOnFourBoard() {
+        Board brd = new Board();
+        String ln = System.lineSeparator();
+        String result = brd.paint(3, 4);
+        assertThat(result, is(String.format("X X%s X %sX X%s X %s", ln, ln, ln, ln)));
+    }
+}
