66f60fd606313a68498797e9eb27ad20f4a3cc66
PesterevViacheslav <pesterevvv@gmail.com>
2018-03-28 18:27:30 +0300

    5.4. Построить пирамиду в псевдографике. [#4412]

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+/**
+ * Class Square решение задачи Части 001. Базовый синтаксис урок 6.
+ *
+ * @author Viacheslav Pesterev (pesterevvv@gmail.com)
+ * @since 28.03.2018
+ * @version 1
+ */
+public class Square {
+
+    /**
+     * Method calculate. Возведение в квадрат элементов массива.
+     * @param bound Размер массива.
+     * @return Массив значений.
+     */
+    public int[] calculate(int bound) {
+        int size = bound <= 0 ? 1 : bound;
+        int[] result = new int[size];
+
+        if (bound > 0) {
+            for (int i = 0; i < bound; i++) {
+                result[i] = (int) Math.pow(i + 1, 2);
+            }
+        } else {
+            result[0] = -1;
+        }
+        System.out.println("result= " + result[0]);
+        return result;
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/array/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Пекет для решение задачи Части 001. Базовый синтаксис урок 6.
+ *
+ * @author Viacheslav Pesterev (pesterevvv@gmail.com)
+ * @since 28.03.2018
+ * @version 1
+ */
+package ru.job4j.array;
