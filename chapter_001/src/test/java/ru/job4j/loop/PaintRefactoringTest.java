af05445b282282659b6fc01bb6bd5e8631f2f0b7
PesterevViacheslav <pesterevvv@gmail.com>
2018-04-03 15:27:14 +0300

    5.5. Рефакторинг кода. [#33459]

---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintRefactoringTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import java.util.StringJoiner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class PaintRefactoringTest Автотесты для задач Части 001. Базовый синтаксис урок 5.
+ *
+ * @author Viacheslav Pesterev (pesterevvv@gmail.com)
+ * @since 03.04.2018
+ * @version 1
+ */
+public class PaintRefactoringTest {
+
+    /**
+     * Тест операции получения псевдографики пирамиды высотой 1
+     */
+    @Test
+    public void checkPaintPiramidHeigthOne() {
+        PaintRefactoring p = new PaintRefactoring();
+        String ln = System.lineSeparator();
+        String result = p.piramid(1);
+        assertThat(result, is(String.format("^%s", ln)));
+    }
+
+    /**
+     * Тест операции получения псевдографики пирамиды высотой 3
+     */
+    @Test
+    public void checkPaintPiramidHeigthThree() {
+        PaintRefactoring p = new PaintRefactoring();
+        String ln = System.lineSeparator();
+        String result = p.piramid(3);
+        assertThat(result, is(new StringJoiner(ln, "", ln)
+                .add("  ^  ")
+                .add(" ^^^ ")
+                .add("^^^^^")
+                .toString()));
+    }
+}
