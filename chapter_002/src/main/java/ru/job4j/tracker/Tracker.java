0b7d2cfddec66e7acc839052a0f4228150b28738
PesterevViacheslav <pesterevvv@gmail.com>
2018-04-27 19:33:12 +0300

    Часть 002. ООП. 3.1 Реализовать класс Tracker [#396]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,103 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+/**
+ * Class Tracker - Обработка заявок. Решение задачи Части 002. ООП. Общая задача на второй модуль.
+ *
+ * @author Viacheslav Pesterev (pesterevvv@gmail.com)
+ * @since 25.04.2018
+ * @version 1
+ */
+public class Tracker {
+    private Item[] items = new Item[100];
+    private int position = 0;
+    /**
+     * Method getName. Получить размер стека заявок.
+     */
+    public int getPosition() {
+        return this.position;
+    }
+    /**
+     * Method add. Добавление заявки.
+     * @param item Заявка.
+     * @return Заявка
+     */
+    public Item add(Item item) {
+        this.items[position++] = item;
+        return item;
+    }
+    /**
+     * Method findById. Поиск заявки по ID.
+     * @param id ID заявки.
+     * @return Заявка
+     */
+    public Item findById(String id) {
+        Item result = new Item();
+        for (int i = 0; i < this.items.length; i++) {
+            if (this.items[i] != null && id.equals(this.items[i].getId())) {
+                result = this.items[i];
+                break;
+            }
+        }
+        return result;
+    }
+    /**
+     * Method findByName. Поиск заявок по названию.
+     * @param name Название заявки.
+     * @return Заявка
+     */
+    public Item[] findByName(String name) {
+        Item[] result = new Item[100];
+        int length = 0;
+        for (int i = 0; i < this.items.length; i++) {
+            if (this.items[i] != null && name.equals(this.items[i].getName())) {
+                result[length] = this.items[i];
+                length++;
+            }
+        }
+        return Arrays.copyOf(result, length);
+    }
+    /**
+     * Method replace. Изменение заявки.
+     * @param id ID заявки.
+     * @param item Заявка.
+     */
+    public void replace(String id, Item item) {
+        Item result = findById(id);
+        result.setName(item.getName());
+        result.setDescription(item.getDescription());
+        result.setChanged(System.currentTimeMillis());
+        result.setId(item.getId());
+        result.setChanged(item.getChanged());
+    }
+    /**
+     * Method replace. Изменение заявки.
+     * @param id ID заявки.
+     */
+    public void delete(String id) {
+        if (this.position > 0) {
+            int positionTmp = 0;
+            Item[] itemsTmp = new Item[100];
+            for (int i = 0; i < this.position; i++) {
+                if (!this.items[i].getId().equals(id)) {
+                    itemsTmp[positionTmp] = this.items[i];
+                    positionTmp++;
+                }
+            }
+            this.items = itemsTmp;
+            this.position--;
+        }
+    };
+    /**
+     * Method findAll. Получение списка текущих заявок.
+     * @return Заявки.
+     */
+    public Item[] findAll() {
+        Item[] result = new Item[this.position];
+        for (int i = 0; i < this.position; i++) {
+            result[i] = this.items[i];
+        }
+        return result;
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,121 @@
+package ru.job4j.tracker;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Class TrackerTest Автотесты для задач Части 002. ООП. Общая задача на второй модуль.
+ *
+ * @author Viacheslav Pesterev (pesterevvv@gmail.com)
+ * @since 18.04.2018
+ * @version 1
+ */
+public class TrackerTest {
+    /**
+     * Тест метода добавления заявки.
+     */
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1", "dsc");
+        tracker.add(item);
+        assertThat(tracker.findAll()[0], is(item));
+        Item item2 = new Item("test2", "dsc2");
+        tracker.add(item2);
+        assertThat(tracker.findAll()[1], is(item2));
+    }
+    /**
+     * Тест метода замены заявки.
+     */
+    @Test
+    public void whenReplaceItemThenReturnNewItem() {
+        Tracker tracker = new Tracker();
+        Item prev = new Item("test1", "dsc1");
+        tracker.add(prev);
+        Item next = new Item("test2", "dsc2");
+        tracker.replace(prev.getId(), next);
+        assertThat(tracker.findById(prev.getId()), is(next));
+    }
+    /**
+     * Тест метода удаления заявки.
+     */
+    @Test
+    public void whenDeleteItemThenDeletedItemNotFound() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1", "dsc1");
+        tracker.add(item);
+        Item item2 = new Item("test2", "dsc1");
+        tracker.add(item2);
+        tracker.delete(item.getId());
+        assertThat(new Item(), is(tracker.findById(item.getId())));
+        assertThat(1, is(tracker.getPosition()));
+    }
+    /**
+     * Тест метода поиска заявки по ID (Найдено).
+     */
+    @Test
+    public void whenFindedById() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1", "dsc1");
+        tracker.add(item);
+        assertThat(item, is(tracker.findById(item.getId())));
+    }
+    /**
+     * Тест метода поиска заявки по ID (Не найдено).
+     */
+    @Test
+    public void whenNotFindedById() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1", "dsc1");
+        tracker.add(item);
+        assertThat(new Item(), is(tracker.findById("111111")));
+    }
+    /**
+     * Тест метода поиска заявки по названию (Найдено).
+     */
+    @Test
+    public void whenFindedByName() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1", "dsc1");
+        tracker.add(item);
+        Item item2 = new Item("test1", "dsc2");
+        tracker.add(item2);
+        Item item3 = new Item("test2", "dsc3");
+        tracker.add(item3);
+        Item[] items = new Item[2];
+        items[0] = item;
+        items[1] = item2;
+        assertThat(items, is(tracker.findByName("test1")));
+    }
+    /**
+     * Тест метода поиска заявки по названию (Не найдено).
+     */
+    @Test
+    public void whenNotFindedByName() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1", "dsc1");
+        tracker.add(item);
+        Item item2 = new Item("test1", "dsc2");
+        tracker.add(item2);
+        Item item3 = new Item("test2", "dsc3");
+        tracker.add(item3);
+        assertThat(new Item[0], is(tracker.findByName("test5")));
+    }
+    /**
+     * Тест метода поиска заявки по названию (Не найдено).
+     */
+    @Test
+    public void whenFindedAllItems() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1", "dsc1");
+        tracker.add(item);
+        Item item2 = new Item("test1", "dsc2");
+        tracker.add(item2);
+        Item item3 = new Item("test2", "dsc3");
+        tracker.add(item3);
+        Item[] items = new Item[3];
+        items[0] = item;
+        items[1] = item2;
+        items[2] = item3;
+        assertThat(items, is(tracker.findAll()));
+    }
+}
