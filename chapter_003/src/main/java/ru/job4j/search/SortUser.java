cc0aab9db6239da6e357d11d0e5a09f9be706131
PesterevViacheslav <pesterevvv@gmail.com>
2018-07-24 16:16:34 +0300

    Часть 003. Collections. Lite. 3.2. Сортировка User с использованием Comparator [#10036]

--- chapter_003/src/main/java/ru/job4j/search/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/search/SortUser.java
@@ -1,16 +1,20 @@
 package ru.job4j.search;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 /**
  * Class PriorityQueue - Очередь задач. Решение задачи Части 003. Collections. Lite.
  * Задача 3.1 Организовать сортировку User.
+ * Задача 3.2. Сортировка User с использованием Comparator
  *
  * @author Viacheslav Pesterev (pesterevvv@gmail.com)
  * @since 22.07.2018
  * @version 1
  */
 public class SortUser {
+    /**
+     * Method sort. Сортировка списка.
+     * @param list Список.
+     * @return Отсортированнный список.
+    */
     public Set<User> sort(List<User> list) {
         TreeSet<User> result = new TreeSet<>();
         for (User user : list) {
@@ -18,4 +22,34 @@ public class SortUser {
         }
         return result;
     }
+    /**
+     * Method sortNameLength. Сортировка списка по длине имени.
+     * @param list Список.
+     * @return Отсортированнный список.
+     */
+    public List<User> sortNameLength(List<User> list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().length() - o2.getName().length();
+            }
+        });
+        return list;
+    }
+    /**
+     * Method sortByAllFields. Сортировка списка по имени и возрасту.
+     * @param list Список.
+     * @return Отсортированнный список.
+     */
+    public List<User> sortByAllFields(List<User> list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int result;
+                result = o1.getName().compareTo(o2.getName());
+                return result != 0 ? result : o1.getAge() - o2.getAge();
+            }
+        });
+        return list;
+}
 }
