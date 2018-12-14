4c6e5a6cba504b6773ee64aee9a54149185f86f2
PesterevViacheslav <pesterevvv@gmail.com>
2018-12-15 01:29:49 +0300

    Junior. Части 002. Multithreading. Pro. 6.1.1 Пин-понг. [#1016]

---
+++ chapter_006/src/main/java/ru/job4j/threads/package-info.java
@@ -0,0 +1,8 @@
+package ru.job4j.threads;
+/**
+ * Пакет для решения задач уровня Junior. Части 002. Multithreading.
+ * Раздел 1. Threads.
+ * @author Viacheslav Pesterev (pesterevvv@gmail.com)
+ * @since 14.12.2018
+ * @version 1
+ */
---
+++ chapter_006/src/test/java/ru/job4j/threads/package-info.java
@@ -0,0 +1,8 @@
+package ru.job4j.threads;
+/**
+ * Пакет автотесты для решения задач уровня Junior. Части 002. Multithreading.
+ * Раздел 1. Threads.
+ * @author Viacheslav Pesterev (pesterevvv@gmail.com)
+ * @since 14.12.2018
+ * @version 1
+ */
