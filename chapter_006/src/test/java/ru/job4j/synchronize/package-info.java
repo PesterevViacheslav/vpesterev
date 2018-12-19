480b76df2e63f3261d684709f517646c305ad7a9
PesterevViacheslav <pesterevvv@gmail.com>
2018-12-19 17:22:13 +0300

    Junior. Части 002. Multithreading. Pro. 6.3.1 класс Count и метод int incremant() [#1102]

--- chapter_006/src/test/java/ru/job4j/synchronize/package-info.java
+++ chapter_006/src/test/java/ru/job4j/synchronize/package-info.java
@@ -1,8 +1,8 @@
-package ru.job4j.threads;
+package ru.job4j.synchronize;
 /**
  * Пакет автотесты для решения задач уровня Junior. Части 002. Multithreading.
- * Раздел 1. Threads.
+ * Раздел 3. Monitor, synchronized.
  * @author Viacheslav Pesterev (pesterevvv@gmail.com)
- * @since 14.12.2018
+ * @since 18.12.2018
  * @version 1
  */
