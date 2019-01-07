92d1d7a2d060ddbfd9c4bc22e1ec810ac0772dc0
PesterevViacheslav <pesterevvv@gmail.com>
2019-01-08 00:45:07 +0300

    Middle. Части 001. Multithreading. Pro. 10.4.1 Реализовать шаблон Producer Consumer. [#1098]

--- chapter_010/src/test/java/ru/job4j/synchronize/package-info.java
+++ chapter_010/src/test/java/ru/job4j/synchronize/package-info.java
@@ -1,6 +1,6 @@
 package ru.job4j.synchronize;
 /**
- * Пакет автотесты для решения задач уровня Junior. Части 002. Multithreading.
+ * Пакет автотесты для решения задач уровня Middle. Части 001. Multithreading.
  * Раздел 3. Monitor, synchronized.
  * @author Viacheslav Pesterev (pesterevvv@gmail.com)
  * @since 18.12.2018
--- chapter_010/src/test/java/ru/job4j/wait/package-info.java
+++ chapter_010/src/test/java/ru/job4j/wait/package-info.java
@@ -1,8 +1,8 @@
-package ru.job4j.jmm;
+package ru.job4j.wait;
 /**
  * Пакет автотестов для решения задач уровня Middle. Части 001. Multithreading.
- * Раздел 2. JMM.
+ * Раздел 4. Wait.
  * @author Viacheslav Pesterev (pesterevvv@gmail.com)
- * @since 14.12.2018
+ * @since 03.01.2019
  * @version 1
  */
