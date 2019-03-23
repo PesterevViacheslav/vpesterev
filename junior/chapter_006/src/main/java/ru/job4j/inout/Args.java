65d2089c5c0141817010427deef5e9703fce3e10
PesterevViacheslav <pesterevvv@gmail.com>
2019-03-23 22:42:07 +0300

    Junior. Части 002. Ввод-Вывод. 6.1.4. Архивировать проект.  [#861]

--- junior/chapter_006/src/main/java/ru/job4j/inout/Args.java
+++ junior/chapter_006/src/main/java/ru/job4j/inout/Args.java
@@ -1,8 +1,11 @@
 package ru.job4j.inout;
 import java.io.File;
 import java.util.ArrayList;
+<<<<<<< HEAD
 import java.util.Arrays;
 
+=======
+>>>>>>> 7f1c972... Junior. Части 002. Ввод-Вывод. 6.1.4. Архивировать проект.  [#861]
 /**
  * Class Args - Аргументы для метода архивации. Решение задач уровня Junior. Части 002. Ввод-Вывод.
  * 6.1.4. Архивировать проект.
@@ -28,14 +31,25 @@ public class Args {
         this.exclude = exclude;
         this.output = output;
     }
+<<<<<<< HEAD
+=======
+
+>>>>>>> 7f1c972... Junior. Части 002. Ввод-Вывод. 6.1.4. Архивировать проект.  [#861]
     /**
      * Method directory. Получение директории которую нужно архивировать.
      *
      * @return Файл.
      */
     public File directory() {
+<<<<<<< HEAD
         return new File(this.directory);
     }
+=======
+        File file = new File(this.directory);
+        return file;
+    }
+
+>>>>>>> 7f1c972... Junior. Части 002. Ввод-Вывод. 6.1.4. Архивировать проект.  [#861]
     /**
      * Method exclude. Получение коллекции исключаемых расширений.
      *
@@ -44,16 +58,30 @@ public class Args {
     public ArrayList<String> exclude() {
         return this.exclude;
     }
+<<<<<<< HEAD
+=======
+
+>>>>>>> 7f1c972... Junior. Части 002. Ввод-Вывод. 6.1.4. Архивировать проект.  [#861]
     /**
      * Method output. Получение директории для размещения архива.
      *
      * @return Файл.
      */
     public File output() {
+<<<<<<< HEAD
         return new File(this.output);
     }
     @Override
     public String toString() {
         return String.join("", "Args{", "directory=", directory, ", exclude=", exclude.toString(), ", output=", output, "}");
+=======
+        File file = new File(this.output);
+        return file;
+    }
+
+    @Override
+    public String toString() {
+        return "Args{" + "directory='" + directory + '\'' + ", exclude=" + exclude + ", output='" + output + '\'' + '}';
+>>>>>>> 7f1c972... Junior. Части 002. Ввод-Вывод. 6.1.4. Архивировать проект.  [#861]
     }
 }
