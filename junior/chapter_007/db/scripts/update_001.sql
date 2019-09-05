49a376dde8323f77d23e1294d19bc912becb4c41
PesterevViacheslav <pesterevvv@gmail.com>
2019-09-05 13:00:04 +0300

    Junior. Части 003. SQL, JDBC. Задача 7.4.4. Liquibase. Интеграционные тесты. Proxy [#95881] (Доработка).

--- junior/chapter_007/db/scripts/update_001.sql
+++ junior/chapter_007/db/scripts/update_001.sql
@@ -1,6 +1,6 @@
-CREATE TABLE ITEM (id SERIAL PRIMARY KEY not null,
-                   item_name TEXT not null,
-                   item_description TEXT,
-                   created TIMESTAMP not null,
-                   changed TIMESTAMP
-                   );
+CREATE TABLE IF NOT EXISTS ITEM (id SERIAL PRIMARY KEY not null,
+                                 item_name TEXT not null,
+                                 item_description TEXT,
+                                 created TIMESTAMP not null,
+                                 changed TIMESTAMP
+                                 );
